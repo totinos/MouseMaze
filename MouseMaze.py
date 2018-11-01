19c3f84143399a76a3566dd41fdf2240eb93c05d
Samuel Brown <sbrow109@vols.utk.edu>
2018-11-01 18:57:26 -0400

    Working on adding capability to generate episodes, added default trap locations

--- MouseMaze.py
+++ MouseMaze.py
@@ -14,6 +14,16 @@ class GRID:
         self.discount = discount
         self.epsilon = epsilon
 
+        # Use explicit names for the different actions for clarity
+        self.UP = 0
+        self.RIGHT = 1
+        self.DOWN = 2
+        self.LEFT = 3
+
+        # Define the starting and ending states
+        self.START = [0, 0]
+        self.GOAL = [7, 7]
+
         # Set up action values and policy as 3-D arrays
         self.action_values = np.zeros((self.grid_dim, self.grid_dim, 4))
         self.policy = np.zeros((self.grid_dim, self.grid_dim, 4))    # USE np.argmax or arr == np.amax(arr)
@@ -24,22 +34,33 @@ class GRID:
         # Generate the locations of the traps randomly, or use default
         # TODO --> Make default less lame
         if self.num_traps == -1:
-            self.num_traps = 10
+            trap_indices = set([19,25,29,35,42,46,49,52,54,59])
     
         # TODO --> Should (0,0) be allowed for a trap position? Probably not if the mouse has to start there
         #      --> What about the cheese? Is it always at the bottom right?
-        trap_indices = set()
-        for i in range(self.num_traps):
-            while (True):
-                x = np.random.randint(self.grid_dim ** 2)
-                if (x not in trap_indices):
-                    trap_indices.add(x)
-                    break
+        else:
+            trap_indices = set()
+            for i in range(self.num_traps):
+                while (True):
+                    x = np.random.randint(self.grid_dim ** 2)
+                    if (x not in trap_indices):
+                        trap_indices.add(x)
+                        break
         for i in trap_indices:
-            self.trap_locations[i//self.grid_dim,i%self.grid_dim] = 1
+            self.trap_locations[i//self.grid_dim][i%self.grid_dim] = 1
         print(self.trap_locations)
 
         # TODO --> Add mouse and cheese locations to the class, differentiate between traps and cheese
+    
+
+    # Take an action according to the policy in the grid
+#    def step(self):
+        # Choose an action according to the policy (but epsilon greedy)
+#        if (np.random.binomial(1, self.epsilon) == 1):
+#            action = np.random.choice(self.action_values[
+
+    # Performs n-step Temporal Difference learning
+#    def sarsa(self):
         
 
 if __name__ == "__main__":
